02933aaacf3a375075f35c2c21b4819cb24cc57d
zhangligang <zhangligang@qq.com>
2021-12-19 23:22:37 +0800

    docker-compose 部署版本

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 let Koa = require('koa');
 let app = new Koa();
 
-// const redis = require('redis');
-// let rds = redis.createClient({url: "redis://redis:6379"});
-// rds.on('connect', ()=> console.log('redis connect ok'))
-// rds.connect();
+const redis = require('redis');
+let rds = redis.createClient({url: "redis://redis:6379"});
+rds.on('connect', ()=> console.log('redis connect ok'))
+rds.connect();
 
 
 let log4js = require('log4js');
@@ -27,15 +27,15 @@ router.all('/hello/:name', async ctx =>{
 	ctx.body = `hello ${name ? name : 'world'}`
 });
 
-// router.all('/redis', async ctx =>{
-// 	let count = await rds.incr("count")
-// 	logger.info(`on test redis page, count ${count}`)
-// 	ctx.body = `on test redis page, 44 count ${count} `
-// });
+router.all('/redis', async ctx =>{
+	let count = await rds.incr("count")
+	logger.info(`on test redis page, count ${count}`)
+	ctx.body = `on test redis page, 44 count ${count} `
+});
 
 app.use(router.routes());
 
-let port = process.env.PORT || 8080;
+let port = 8080;
 try{
 	app.listen(8080);
 	logger.info('Server started successfully and listened on '+ port +'\n'+'http://localhost:'+port);
---
+++ readme.md
@@ -0,0 +1,6 @@
+测试运行
+执行如下命令
+
+# 1. 运行服务 docker-compose up -d
+# 2. 停止服务 docker-compose down
+# 3. 构建镜像 docker-compose build 
